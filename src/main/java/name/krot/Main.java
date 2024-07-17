94e30c0834d766edb5a9788e9240eb417015a860
Ovcharenko.AA <Ovcharenko.AA@esphere.ru>
2024-07-17 03:06:55 +0300

    fix otus

--- src/main/java/name/krot/Main.java
+++ src/main/java/name/krot/Main.java
@@ -2,6 +2,6 @@ package name.krot;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello otus!");
     }
 }
